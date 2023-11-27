bc20124991935a279408e4279c49883d7d4edd6f
C.Nelhomme <64921289+D3ller@users.noreply.github.com>
2023-11-27 16:12:15 +0100

    rendu.js

--- public/assets/js/rendu.js
+++ public/assets/js/rendu.js
@@ -56,3 +56,28 @@ submit.addEventListener("click", function (event) {
     }
 
 });
+
+document.addEventListener("DOMContentLoaded", function (event) {
+
+    fetch("../assets/json/user.json")
+        .then(response => response.json())
+        .then(data => {
+            var userId = localStorage.getItem("id");
+            data["utilisateurs"].forEach(user => {
+                if(user.id === userId){
+                    document.getElementById("username").innerHTML = user.prenom;
+                    var imgs = document.getElementsByClassName("img");
+                    for(var i = 0; i < imgs.length; i++){
+                        imgs[i].style.backgroundImage = "url('" + user.image + "')";
+                        imgs[i].style.backgroundSize = "cover";
+                    }
+                    document.getElementById('name').innerHTML = user.prenom + " " + user.nomDeFamille;
+                    document.getElementById('season').innerHTML = "CM "+ user.promo;
+                    document.getElementById('td').innerHTML = "TD "+ user.groupeTD;
+                    document.getElementById('tp').innerHTML = "TP " + user.groupeTP;
+                    document.getElementById('email').innerHTML = user.prenom.toLowerCase() + "." + user.nomDeFamille.toLowerCase() + "@etudiant.univ-reims.fr";
+                }
+            });
+        })
+
+});
